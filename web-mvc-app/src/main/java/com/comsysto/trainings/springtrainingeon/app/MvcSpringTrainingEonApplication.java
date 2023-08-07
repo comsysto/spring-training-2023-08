65e0840aea646a535260dca5a88a625800509fbb
Lars Schweisthal <larsschweisthal@pw-mbp2.local>
2023-08-07 10:42:44 +0200

    testcommit

--- web-mvc-app/src/main/java/com/comsysto/trainings/springtrainingeon/app/MvcSpringTrainingEonApplication.java
+++ web-mvc-app/src/main/java/com/comsysto/trainings/springtrainingeon/app/MvcSpringTrainingEonApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableCaching
 public class MvcSpringTrainingEonApplication {
 
-    // test
     public static void main(String[] args) {
         SpringApplication.run(MvcSpringTrainingEonApplication.class, args);
     }
