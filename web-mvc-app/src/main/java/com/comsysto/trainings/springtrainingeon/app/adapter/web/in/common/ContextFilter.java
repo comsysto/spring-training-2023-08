4f360ddc64fe08f68ab34f2086e9e8fc9d2c4690
okrammer <1805495+okrammer@users.noreply.github.com>
2023-08-09 10:12:45 +0200

    added reset of context

--- web-mvc-app/src/main/java/com/comsysto/trainings/springtrainingeon/app/adapter/web/in/common/ContextFilter.java
+++ web-mvc-app/src/main/java/com/comsysto/trainings/springtrainingeon/app/adapter/web/in/common/ContextFilter.java
@@ -1,41 +1,41 @@
 package com.comsysto.trainings.springtrainingeon.app.adapter.web.in.common;
 
-import java.io.IOException;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.comsysto.trainings.springtrainingeon.app.port.context.out.BearerToken;
 import com.comsysto.trainings.springtrainingeon.app.port.context.out.Context;
 import com.comsysto.trainings.springtrainingeon.app.port.context.out.ContextRepository;
 import com.comsysto.trainings.springtrainingeon.app.port.context.out.RequestId;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ContextFilter extends OncePerRequestFilter
-{
-	private final ContextRepository contextRepository;
+public class ContextFilter extends OncePerRequestFilter {
+    private final ContextRepository contextRepository;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-		throws ServletException, IOException
-	{
-		String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-		String requestId = request.getHeader("requestId");
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String requestId = request.getHeader("requestId");
 
-		Context context = new Context(new RequestId(requestId), new BearerToken(authHeader));
-		contextRepository.setContext(context);
+        Context context = new Context(new RequestId(requestId), new BearerToken(authHeader));
+        contextRepository.setContext(context);
 
-		log.info("auth token: {}", authHeader);
-		filterChain.doFilter(request, response);
-	}
+        log.info("auth token: {}", authHeader);
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            contextRepository.reset();
+        }
+    }
 }
